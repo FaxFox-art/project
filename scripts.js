7ad8f427d97e05f803720cd91d8ba635b719825a
FaxFox-art <FaxFox2@icloud.com>
2024-08-07 10:18:27 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,5 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
+            const hamburgerMenu = document.getElementById('hamburger-menu');
+            hamburgerMenu.addEventListener('click', () => {
+                const nav = document.querySelector('nav ul');
+                nav.classList.toggle('open');
+            });
         });
 });
